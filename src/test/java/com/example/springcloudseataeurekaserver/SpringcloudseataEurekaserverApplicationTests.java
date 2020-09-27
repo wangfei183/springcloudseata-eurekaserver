86a97ae2b3b6fd361c7733233d2b408ddc6889d5
wangfei183 <18339813925@163.com>
2020-09-27 17:28:08 +0800

    增加springcloudseata项目eureka服务器端

---
+++ src/test/java/com/example/springcloudseataeurekaserver/SpringcloudseataEurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudseataeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudseataEurekaserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
